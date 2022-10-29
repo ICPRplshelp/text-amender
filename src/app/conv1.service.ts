6ac0e02e67ae91b041c33523a6ecb41b5780d19b
ICPRplshelp <93059453+ICPRplshelp@users.noreply.github.com>
2022-10-28 23:21:43 -0400

    Amend files

--- src/app/conv1.service.ts
+++ src/app/conv1.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
@@ -15,11 +15,23 @@ export class Conv1Service {
    */
   fixUpAligns(text: string): string {
     let t2 = text.split("\\bigm");
-    let t3 = t2.map(s => `& ${s} \\\\`);
+    let t3 = t2.map(s => `& ${this.fixUpBadNotation(s)} \\\\`);
     let t4 = t3.join('\n');
-    let t5 = "\\begin{align*}\n" + t4 + "\n\\end{align*}\n"
-    return t5;
+    return "\\begin{align*}\n" + t4 + "\n\\end{align*}\n";
 
+  }
+
+  fixUpBadNotation(text: string): string {
+    const tli: string[][] = [
+      ['\\{', '\\lbrace'],
+      ['\\}', '\\rbrace'],
+      ['\\emsp', '\\quad']
+    ]
+
+    for (let tl of tli) {
+      text = text.replace(tl[0], tl[1]);
+    }
+    return text;
   }
 
 
