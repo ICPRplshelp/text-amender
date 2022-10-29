5f63d0bf15e70a87451ff3c9815a2783528dea65
ICPRplshelp <93059453+ICPRplshelp@users.noreply.github.com>
2022-10-28 22:06:41 -0400

    project_setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StandardComponent } from './standard/standard.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import { FormsModule } from '@angular/forms';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+
+import {ClipboardModule} from '@angular/cdk/clipboard';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StandardComponent,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    MatInputModule,
+    ClipboardModule,
+    MatSnackBarModule,
+    AppRoutingModule,
+    FormsModule      ,
+    MatButtonModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
