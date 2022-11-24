24966d4b0707538019f966ca62be79331a8ead29
ICPRplshelp <93059453+ICPRplshelp@users.noreply.github.com>
2022-11-24 14:38:28 -0500

    asmstuff

--- src/app/conv1.service.ts
+++ src/app/conv1.service.ts
@@ -14,7 +14,7 @@ export class Conv1Service {
    * @param text the input text.
    */
   fixUpAligns(text: string): string {
-    let t2 = text.split("\\bigm");
+    let t2 = text.replaceAll('\n', '\\bigm').split("\\bigm");
     let t3 = t2.map(s => `& ${this.fixUpBadNotation(s)} \\\\`);
     let t4 = t3.join('\n');
     return "\\begin{align*}\n" + t4 + "\n\\end{align*}\n";
@@ -44,6 +44,65 @@ export class Conv1Service {
       text = text.replaceAll(tl[0], tl[1]);
     }
     return text;
+  }
+
+
+  saveMeFull(inputString: string): string {
+    console.log('SAVE ME');
+    let toSave = this.extractNumbers(inputString);
+    return this.saveMe(toSave);
+  }
+
+  /**
+   * return a set of all digits 0-9 that appear in
+   * inputString.
+   * @param inputString the input string
+   */
+  extractNumbers(inputString: string): number[] {
+
+    //
+    // This is a regular expression that matches
+    // any digit 0-9.
+    //
+    const re = /\d/g;
+    let m;
+    let result: number[] = [];
+    while ((m = re.exec(inputString)) !== null) {
+      // This is necessary to avoid infinite loops with zero-width matches
+      if (m.index === re.lastIndex) {
+        re.lastIndex++;
+      }
+
+      // The result can be accessed through the `m`-variable.
+      m.forEach((match, groupIndex) => {
+        result.push(Number(match));
+      });
+    }
+    return result;
+
+
+  }
+
+
+  saveMe(toSave: number[]): string {
+    const strIndent = "    ";
+    toSave.sort();
+    let cStr: string = strIndent + "# PROLOGUE ---------- #\n";
+    cStr += (strIndent + "addi $sp, $sp, -4\n"
+    + strIndent + "sw $ra, 0($sp)  # perserve the return address\n");
+
+    for(let i of toSave){
+      cStr += (strIndent + `addi $sp, $sp, -4\n` + strIndent + `sw $s${i}, 0($sp)  # perserve $sp${i}\n`);
+    }
+    cStr += (strIndent + "\n# EPILOGUE ---------- #\n");
+    toSave.reverse();
+    for(let i of toSave){
+      cStr += (strIndent + `sw $s${i}, 0($sp)  # load back $sp${i}\n` + strIndent + `addi $sp, $sp, 4\n`); 
+    }
+
+    cStr += (strIndent + "sw $ra, 0($sp)  # load back the return address\n" + strIndent + "addi $sp, $sp, 4\n");
+
+    return cStr;
   }
 
 
--- src/app/standard/standard.component.ts
+++ src/app/standard/standard.component.ts
@@ -17,8 +17,8 @@ export class StandardComponent implements OnInit {
   editableText: string = "";
   displayText: string = "";
 
-  hintText: string = "Stacked-raw MS Word-LaTeX code";
-  bigHintText: string = "Converts MS Word stacked equations into LaTeX-compatible code. Open a new equation box, set equation mode to LaTeX, select the series of stacked equations you want to copy, hit copy, and paste it in the text box. Then, click on process me.";
+  hintText: string = "Microsoft Equation LaTeX Multiline Linear Format";
+  bigHintText: string = "Converts MS Word stacked equations into LaTeX-compatible code. Open a new equation box, set equation mode to LaTeX, select the series of stacked equations you want to copy, hit copy, and paste it in the text box. Then, click on process me. If you're using the Align option, equations should be stacked with SHIFT+ENTER. To minimize errors, I strongly recommend starting each new line with an operator. MS Word's copy-paste results are not consistent so expect errors.";
 
   copied: boolean = false;
   private _currentMode: string = "align";
@@ -29,12 +29,16 @@ export class StandardComponent implements OnInit {
     this._currentMode = value;
     switch(value.toLowerCase().replace(' ', '')){
       case 'align':
-        this.hintText = "Stacked-raw MS Word-LaTeX code";
-        this.bigHintText = "Converts MS Word stacked equations into LaTeX-compatible code. Open a new equation box, set equation mode to LaTeX, select the series of stacked equations you want to copy, hit copy, and paste it in the text box. Then, click on process me.";
+        this.hintText = "Microsoft Equation LaTeX Multiline Linear Format";
+        this.bigHintText = "Converts MS Word stacked equations into LaTeX-compatible code. Open a new equation box, set equation mode to LaTeX, select the series of stacked equations you want to copy, hit copy, and paste it in the text box. Then, click on process me. If you're using the Align option, equations should be stacked with SHIFT+ENTER. To minimize errors, I strongly recommend starting each new line with an operator. MS Word's copy-paste results are not consistent so expect errors.";
         break;
       case 'unicodecopy':
-        this.hintText = "UnicodeMath code.";
+        this.hintText = "UnicodeMath code";
         this.bigHintText = "Type an equation in MS Word (keep the mode on UnicodeMath), and copy it to here. This converts it into a form that Google's search can read. Not all operators are supported, so only use elementary arithmetic, complex numbers, and the choose operator";
+        break;
+      case 'asmprologue':
+        this.hintText = "Numbers only, maybe not containing duplicates";
+        this.bigHintText = "The assembly prologue generator that is likely not your version of assembly";
         break;
       default:
         this.hintText = 'TBA';
@@ -42,7 +46,7 @@ export class StandardComponent implements OnInit {
     }
   }
   hint: string = "";
-  eqOptions: string[] = ["align", "unicodeCopy"];
+  eqOptions: string[] = ["align", "unicodeCopy", "asmPrologue"];
 
   upperCaseFirst(text: string): string {
     if (text.length === 0){
@@ -53,6 +57,7 @@ export class StandardComponent implements OnInit {
   }
 
   updateDisplayText(): void {
+    console.log(this.currentMode.toLowerCase().replace(' ', ''));
 
     switch(this.currentMode.toLowerCase().replace(' ', '')){
     case "align":
@@ -60,7 +65,13 @@ export class StandardComponent implements OnInit {
       break;
     case "unicodecopy":
       this.displayText = this.c1s.fixUnicodeEquations(this.editableText);
+      break;
+    case "asmprologue":
+      this.displayText = this.c1s.saveMeFull(this.editableText);
+      break;
     }
+    console.log(this.displayText);
+    
   }
 
   displayCopyMessage(): void {
